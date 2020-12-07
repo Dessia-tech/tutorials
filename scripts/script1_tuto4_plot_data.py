c14770f4c0035531549d7f5ecfb2bcc2967e8751
dumouchel <dumouchel@dessia.tech>
2020-12-08 00:00:38 +0100

    update

--- scripts/script1_tuto4_plot_data.py
+++ scripts/script1_tuto4_plot_data.py
@@ -7,3 +7,11 @@ g1 = tuto.Graph(2, 50)
 pdt = g1.plot_data()
 plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas',
                       debug_mode=True)
+
+s1 = tuto.ScatterPlot()
+plot_data.plot_canvas(plot_data_object=s1.plot_data(), canvas_id='canvas',
+                      debug_mode=True)
+
+# from dessia_api_client import Client
+# c = Client(api_url='https://api.safran-es.dessia.tech')
+# r = c.create_object_from_python_object(g1)
