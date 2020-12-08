0565c893d41927fa680e1b00279e331778ea0285
dumouchel <dumouchel@dessia.tech>
2020-12-08 01:03:12 +0100

    update

--- scripts/script1_tuto4_plot_data.py
+++ scripts/script1_tuto4_plot_data.py
@@ -8,9 +8,22 @@ pdt = g1.plot_data()
 plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas',
                       debug_mode=True)
 
-s1 = tuto.ScatterPlot()
+s1 = tuto.ScatterPlot(2, 2)
 plot_data.plot_canvas(plot_data_object=s1.plot_data(), canvas_id='canvas',
                       debug_mode=True)
+
+p1 = tuto.ParallelPlot(2, 2)
+plot_data.plot_canvas(plot_data_object=p1.plot_data(), canvas_id='canvas',
+                      debug_mode=True)
+
+m1 = tuto.MultiPlot(2, 2)
+plot_data.plot_canvas(plot_data_object=m1.plot_data(), canvas_id='canvas',
+                      debug_mode=True)
+
+shape1 = tuto.SimpleShape(3, 2)
+plot_data.plot_canvas(plot_data_object=shape1.plot_data(), canvas_id='canvas',
+                      debug_mode=True)
+
 
 # from dessia_api_client import Client
 # c = Client(api_url='https://api.demo.dessia.tech')
