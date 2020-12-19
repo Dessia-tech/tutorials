0ac16428cc03d0e7a5884966bce45e924f6a4288
dumouchel <dumouchel@dessia.tech>
2020-12-19 22:05:58 +0100

    improve tuto4

--- scripts/script1_tuto4_plot_data.py
+++ scripts/script1_tuto4_plot_data.py
@@ -4,34 +4,29 @@ import volmdlr as vm
 
 g1 = tuto.Graph(2, 50)
 pdt = g1.plot_data()
-plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas',
-                      debug_mode=True)
+plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas')
 
-g1 = tuto.Graphs(2, 50)
-pdt = g1.plot_data()
-plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas',
-                      debug_mode=True)
+g2 = tuto.Graphs(2, 50)
+pdt = g2.plot_data()
+plot_data.plot_canvas(plot_data_object=pdt, canvas_id='canvas')
 
 s1 = tuto.ScatterPlot(2, 2)
-plot_data.plot_canvas(plot_data_object=s1.plot_data(), canvas_id='canvas',
-                      debug_mode=True)
+plot_data.plot_canvas(plot_data_object=s1.plot_data(), canvas_id='canvas')
 
 p1 = tuto.ParallelPlot(2, 2)
-plot_data.plot_canvas(plot_data_object=p1.plot_data(), canvas_id='canvas',
-                      debug_mode=True)
+plot_data.plot_canvas(plot_data_object=p1.plot_data(), canvas_id='canvas')
 
 m1 = tuto.MultiPlot(2, 2)
-plot_data.plot_canvas(plot_data_object=m1.plot_data(), canvas_id='canvas',
-                      debug_mode=True)
+plot_data.plot_canvas(plot_data_object=m1.plot_data(), canvas_id='canvas')
 
 shape1 = tuto.SimpleShape(3, 2)
-plot_data.plot_canvas(plot_data_object=shape1.plot_data(), canvas_id='canvas',
-                      debug_mode=True)
+plot_data.plot_canvas(plot_data_object=shape1.plot_data(), canvas_id='canvas')
 
 
 from dessia_api_client import Client
-c = Client(api_url='https://api.demo.dessia.tech')
+c = Client(api_url='https://api.stelia.dessia.tech')
 r = c.create_object_from_python_object(g1)
+r = c.create_object_from_python_object(g2)
 r = c.create_object_from_python_object(s1)
 r = c.create_object_from_python_object(p1)
 r = c.create_object_from_python_object(m1)
