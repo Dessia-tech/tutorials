3be55f67f49a25044f45637ea4f05b5c6a0f61e1
WirajanDASILVA <wirajan.klebson@gmail.com>
2021-03-11 19:26:08 +0100

    Create script9_simple_3ratios_gearbox.py

---
+++ scripts/script9_simple_3ratios_gearbox.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 11:29:57 2021
+
+@author: wirajan
+"""
+import tutorials.Tutoriel_BV_3rapports as objects
+
+
+
+speed = [[800, 1800], [800,1800], [1800,3200], [800, 3200], [800, 3200], [800, 3200], [800, 1100],[1100, 3200], [1100, 3200], [1600, 3200], [1600, 2400], [2400, 3200],[2000,3200]]
+torque= [[0,12],[12,18],[0,18],[18,25], [25,35], [35,50], [55, 80],[50,70],[70,95],[85,105], [105,125], [105,125],[125,160]]
+efficiency = [0.14, 0.18, 0.18, 0.22, 0.26, 0.29, 0.32,0.32, 0.34, 0.35,0.35, 0.36, 0.32]
+data = objects.Data(speed = speed, torque = torque, efficiency = efficiency)
+motor = objects.Motor(speed=1000, torque = 80, data = data)
+gearbox = objects.GearBox(motor = motor)
+speeds = [784, 1100, 1792]
+torques = [475, 340, 200]
+
+optimizer = objects.GearBoxOptimizer(gearbox = gearbox, ratios_min_max=[0.5,4.2], speeds = speeds, torques = torques, number_ratios=3)
+
+results = optimizer.optimize(10000)
+for result in results[0]:
+    print('ratios:',result.ratios)
+    print('eficiencies: ', result.efficiencies)
+    print('\n')
+    
+print('\n')
+
+print(results[1])
+print('\n')
+
+functional_min=min(results[1])
+gearbox_ratios=[]
+for i, (gearbox,functional) in enumerate(zip(results[0],results[1])):
+    if functional == functional_min:
+        gearbox_ratios.append(gearbox.ratios)
+        
+         
+for ratio in gearbox_ratios:
+    print('raios: ',ratio)
+    print('eficiencies: ', result.efficiencies)
+    print('\n')
