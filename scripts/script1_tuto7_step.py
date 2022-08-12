464b92f3ea1119cb9d634b561b8f0c398e857283
younesdessia <younes.belabid@dessia.io>
2022-08-12 11:56:50 +0200

    update script_step / datas

--- scripts/script1_tuto7_step.py
+++ scripts/script1_tuto7_step.py
@@ -6,13 +6,30 @@
 
 import volmdlr as vm
 import volmdlr.step
+import volmdlr.step as STEP
+from pathlib import Path
 
-step = volmdlr.step.Step('export_bis.stp')
-model = step.to_volume_model()
-print(model.primitives)
 
-model.babylonjs()
+# step = STEP.Step('export_bis.stp')
+# model = step.to_volume_model()
+# print(model.primitives)
+#
+# model.babylonjs()
+#
+# model2 = model.copy()
+#
+# assert model == model2
 
-model2 = model.copy()
 
-assert model == model2
+def from_step_file(path):
+    step = STEP.Step.from_file(path)
+    model = step.to_volume_model()
+    model.babylonjs()
+
+
+DIR = Path(__file__).resolve().parent / 'datas' / 'model_step.step'
+# EX :  DIR = Path.home() / 'Documents' / ...
+
+
+from_step_file(path=DIR)
+
