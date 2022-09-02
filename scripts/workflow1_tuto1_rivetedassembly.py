8bbf9fe0a21fb156ed6abc7e82c4e73290edc989
younesdessia <younes.belabid@dessia.io>
2022-09-02 19:21:46 +0200

    update workflow tuto 1

--- scripts/workflow1_tuto1_rivetedassembly.py
+++ scripts/workflow1_tuto1_rivetedassembly.py
@@ -9,23 +9,25 @@ import tutorials.tutorial1_rivetedassembly as tuto
 import plot_data.core as plot_data
 import volmdlr as vm
 from dessia_api_client.users import PlatformUser
-import dessia_common.workflow as wf
+from dessia_common.workflow.blocks import InstantiateModel, ModelMethod, MethodType, MultiPlot
+from dessia_common.workflow.core import Workflow, Pipe
 
-block_generator = wf.InstantiateModel(tuto.Generator, name='Generator')
 
-generate_method = wf.MethodType(class_=tuto.Generator, name='generate')
-block_generate = wf.ModelMethod(method_type=generate_method, name='generate')
+block_generator = InstantiateModel(tuto.Generator, name='Generator')
+
+block_generate = ModelMethod(method_type=MethodType(tuto.Generator, 'generate'), name='generate')
+
 
 list_attribute1 = ['number_rivet1', 'number_rivet2', 'number_rivet', 'mass', 'pressure_applied', 'fatigue_resistance']
-display_reductor = wf.MultiPlot(list_attribute1, 1, name='Display Reductor')
+display_reductor = MultiPlot(list_attribute1, 1, name='Display Rivet Assembly')
 
 block_workflow = [block_generator, block_generate, display_reductor]
 
-pipe_worflow = [wf.Pipe(block_generator.outputs[0], block_generate.inputs[0]),
-                wf.Pipe(block_generate.outputs[0], display_reductor.inputs[0])]
+pipe_worflow = [Pipe(block_generator.outputs[0], block_generate.inputs[0]),
+                Pipe(block_generate.outputs[0], display_reductor.inputs[0])]
 
-workflow = wf.Workflow(block_workflow, pipe_worflow, block_generate.outputs[0])
-
+workflow = Workflow(block_workflow, pipe_worflow, block_generate.outputs[0])
+workflow.plot()
 p1 = tuto.Panel(1, 1, 0.01)
 p2 = tuto.Panel(1.1, 1, 0.01)
 r1 = tuto.Rivet(0.01, 0.05, 0.012, 0.005)
