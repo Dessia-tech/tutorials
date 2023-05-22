d5074e1309ec4f028eda20a67fc9323420ffb52f
younesdessia <younes.belabid@dessia.io>
2023-05-22 15:11:03 +0200

    remove test

--- scripts/tests.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-for script_name in os.listdir():
-    if script_name.endswith('.py'):
-        print('\n## Executing script {}'.format(script_name))
-        exec(open(script_name).read())
