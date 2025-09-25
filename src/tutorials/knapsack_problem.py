6616028b3cb31b0695fd104691c58638a3c30fdb
Ghislain Jézéquel <jezequel@dessia.io>
2025-09-25 17:38:08 +0200

    chore: reset vm and plot_data

--- src/tutorials/knapsack_problem.py
+++ src/tutorials/knapsack_problem.py
@@ -1,19 +1,18 @@
 from __future__ import annotations
 from itertools import combinations
-from typing import ClassVar, Literal
+from typing import ClassVar
 
-from dessia_common.core import DessiaObject, Model
+from dessia_common.models.core import Model
 from dessia_common.files import BinaryFile
-# from datatools.dataset import Dataset
+from datatools.dataset import Dataset
 from dessia_common.decorators import cad_view, plot_data_view, markdown_view
 from dessia_common.models.decorators import modelclass, model_property
-# from plot_data import PrimitiveGroup, SurfaceStyle, Text, TextStyle
-# from plot_data.colors import BLACK, Color
-# from volmdlr import O3D, X3D, Y3D, Z3D, Frame3D, Point2D
-# from volmdlr.core import VolumeModel
-# from volmdlr.shapes import Solid
-# import volmdlr.step as vms
-# from volmdlr.wires import ClosedPolygon2D
+from plot_data.core import PrimitiveGroup, SurfaceStyle, Text, TextStyle
+from plot_data.colors import BLACK, Color
+from volmdlr.core import VolumeModel, O3D, X3D, Y3D, Z3D, Frame3D, Point2D
+from volmdlr.shapes import Solid
+import volmdlr.step as vms
+from volmdlr.wires import ClosedPolygon2D
 
 from dessia_common.typings import KeyOf
 
@@ -54,52 +53,52 @@ class Item(Model):
     def rgb(self) -> tuple[float, float, float]:  # Would be awesome to compute type from constant values
         return ITEM_COLORS[self.color]
 
-    # def volmdlr_primitives(self, z_offset: float = 0., reference_path: str = "#", **kwargs):
-    #     height_vector = self.mass * Z3D / 2
-    #     frame = Frame3D(origin=O3D + height_vector / 2 + z_offset * Z3D,
-    #                     u=X3D,
-    #                     v=Y3D,
-    #                     w=Z3D,
-    #                     name=f"frame {self.name}")
-    #     solid = Solid.make_box(length=1, width=1, height=height_vector.norm(), frame=frame,
-    #                                  frame_centered=True, name=f"block {self.name}")
-    #     solid.reference_path = reference_path
-    #     solid.color = self.rgb
-    #     return [solid]
+    def volmdlr_primitives(self, z_offset: float = 0., reference_path: str = "#", **kwargs):
+        height_vector = self.mass * Z3D / 2
+        frame = Frame3D(origin=O3D + height_vector / 2 + z_offset * Z3D,
+                        u=X3D,
+                        v=Y3D,
+                        w=Z3D,
+                        name=f"frame {self.name}")
+        solid = Solid.make_box(length=1, width=1, height=height_vector.norm(), frame=frame,
+                                     frame_centered=True, name=f"block {self.name}")
+        solid.reference_path = reference_path
+        solid.color = self.rgb
+        return [solid]
 
-    # @cad_view("Item CAD")
-    # def cadview(self):
-    #     return VolumeModel(self.volmdlr_primitives()).babylon_data()
-    #
-    # @plot_data_view("2D display for Item")
-    # def display_2d(self, y_offset: float = 0., reference_path: str = "#"):
-    #     contour = ClosedPolygon2D([
-    #         Point2D(-0.5, -0.5 + y_offset),
-    #         Point2D(0.5, -0.5 + y_offset),
-    #         Point2D(0.5, 0.5 + y_offset),
-    #         Point2D(-0.5, 0.5 + y_offset)])
-    #     contour.reference_path = reference_path
-    #     surface_style = SurfaceStyle(color_fill=Color(red=self.rgb[0],green=self.rgb[1], blue=self.rgb[2]))
-    #     primitive1 = contour.plot_data(surface_style=surface_style)
-    #     text_style = TextStyle(text_color=BLACK,
-    #                            font_size=None,
-    #                            text_align_x="center",
-    #                            text_align_y="middle")
-    #     primitive2 = Text(comment=f"{self.mass} kg",
-    #                       position_x=0,
-    #                       position_y=0.4 + y_offset,
-    #                       text_style=text_style,
-    #                       text_scaling=True,
-    #                       max_width=0.5,
-    #                       multi_lines=False)
-    #     primitive3 = Text(comment=f"{self.price} €",
-    #                       position_x=0,
-    #                       position_y=-0.1 + y_offset,
-    #                       text_style=text_style,
-    #                       text_scaling=True,
-    #                       max_width=0.5,
-    #                       multi_lines=False)
-    #     return PrimitiveGroup(primitives=[primitive1, primitive2, primitive3])
+    @cad_view("Item CAD")
+    def cadview(self):
+        return VolumeModel(self.volmdlr_primitives()).babylon_data()
+
+    @plot_data_view("2D display for Item")
+    def display_2d(self, y_offset: float = 0., reference_path: str = "#"):
+        contour = ClosedPolygon2D([
+            Point2D(-0.5, -0.5 + y_offset),
+            Point2D(0.5, -0.5 + y_offset),
+            Point2D(0.5, 0.5 + y_offset),
+            Point2D(-0.5, 0.5 + y_offset)])
+        contour.reference_path = reference_path
+        surface_style = SurfaceStyle(color_fill=Color(red=self.rgb[0],green=self.rgb[1], blue=self.rgb[2]))
+        primitive1 = contour.plot_data(surface_style=surface_style)
+        text_style = TextStyle(text_color=BLACK,
+                               font_size=None,
+                               text_align_x="center",
+                               text_align_y="middle")
+        primitive2 = Text(comment=f"{self.mass} kg",
+                          position_x=0,
+                          position_y=0.4 + y_offset,
+                          text_style=text_style,
+                          text_scaling=True,
+                          max_width=0.5,
+                          multi_lines=False)
+        primitive3 = Text(comment=f"{self.price} €",
+                          position_x=0,
+                          position_y=-0.1 + y_offset,
+                          text_style=text_style,
+                          text_scaling=True,
+                          max_width=0.5,
+                          multi_lines=False)
+        return PrimitiveGroup(primitives=[primitive1, primitive2, primitive3])
 
 
 @modelclass
@@ -113,29 +112,29 @@ class Items(Model):
 
     _standalone_in_db: ClassVar[bool] = True
 
-    # def volmdlr_primitives(self, reference_path: str = "#", **kwargs):
-    #     primitives = []
-    #     z_offset = 0
-    #     for i, item in enumerate(self.items):
-    #         item_primitives = item.volmdlr_primitives(z_offset=z_offset, reference_path=f"{reference_path}/items/{i}")
-    #         primitives.extend(item_primitives)
-    #         z_offset += item.mass / 2 + 0.05
-    #     return primitives
-    #
-    # @cad_view("Knapsack CAD")
-    # def cadview(self, reference_path: str = "#"):
-    #     return VolumeModel(self.volmdlr_primitives(reference_path=reference_path)).babylon_data()
-    #
-    # @plot_data_view("2D display for KnapsackPackage")
-    # def display_2d(self, reference_path: str = "#"):
-    #     primitives = []
-    #     y_offset = 0
-    #     for i, item in enumerate(self.items):
-    #         primitive_groups = item.display_2d(y_offset=y_offset)
-    #         primitives.extend(primitive_groups.primitives)
-    #         y_offset += 1.1
-    #
-    #     return PrimitiveGroup(primitives=primitives)
+    def volmdlr_primitives(self, reference_path: str = "#", **kwargs):
+        primitives = []
+        z_offset = 0
+        for i, item in enumerate(self.items):
+            item_primitives = item.volmdlr_primitives(z_offset=z_offset, reference_path=f"{reference_path}/items/{i}")
+            primitives.extend(item_primitives)
+            z_offset += item.mass / 2 + 0.05
+        return primitives
+
+    @cad_view("Knapsack CAD")
+    def cadview(self, reference_path: str = "#"):
+        return VolumeModel(self.volmdlr_primitives(reference_path=reference_path)).babylon_data()
+
+    @plot_data_view("2D display for KnapsackPackage")
+    def display_2d(self, reference_path: str = "#"):
+        primitives = []
+        y_offset = 0
+        for i, item in enumerate(self.items):
+            primitive_groups = item.display_2d(y_offset=y_offset)
+            primitives.extend(primitive_groups.primitives)
+            y_offset += 1.1
+
+        return PrimitiveGroup(primitives=primitives)
 
 
 @modelclass
@@ -151,33 +150,33 @@ class Knapsack(Model):
 
     _standalone_in_db = True
 
-    # def volmdlr_primitives(self):
-    #     height_vector = (self.allowed_mass + 0.5) * Z3D / 2
-    #     frame = Frame3D(origin=O3D + height_vector / 2,
-    #                     u=X3D,
-    #                     v=Y3D,
-    #                     w=Z3D,
-    #                     name=f"frame {self.name}")
-    #     primitives = [Solid.make_box(length=1.1, width=1.1, height=height_vector.norm(), frame=frame,
-    #                                  frame_centered=True, name=f"block {self.name}")]
-    #     primitives[0].alpha = 0.4
-    #     return primitives
-    #
-    # @classmethod
-    # def from_step_stream(cls, file_component: BinaryFile) -> Knapsack:
-    #     """
-    #     Method used to load a Knapsack in step format
-    #     :param file_component: Knapsack defined from step file
-    #     :return: Knapsack
-    #     """
-    #     file = vms.Step.from_stream(file_component)
-    #     volume = file.to_volume_model()
-    #     allowed_mass = 2 * (volume.primitives[0].bounding_box.size[2] - 0.1) - 0.5
-    #     return cls(allowed_mass=allowed_mass)
-    #
-    # @cad_view("Knapsack CAD")
-    # def cadview(self):
-    #     return VolumeModel(self.volmdlr_primitives()).babylon_data()
+    def volmdlr_primitives(self):
+        height_vector = (self.allowed_mass + 0.5) * Z3D / 2
+        frame = Frame3D(origin=O3D + height_vector / 2,
+                        u=X3D,
+                        v=Y3D,
+                        w=Z3D,
+                        name=f"frame {self.name}")
+        primitives = [Solid.make_box(length=1.1, width=1.1, height=height_vector.norm(), frame=frame,
+                                     frame_centered=True, name=f"block {self.name}")]
+        primitives[0].alpha = 0.4
+        return primitives
+
+    @classmethod
+    def from_step_stream(cls, file_component: BinaryFile) -> Knapsack:
+        """
+        Method used to load a Knapsack in step format
+        :param file_component: Knapsack defined from step file
+        :return: Knapsack
+        """
+        file = vms.Step.from_stream(file_component)
+        volume = file.to_volume_model()
+        allowed_mass = 2 * (volume.primitives[0].bounding_box.size[2] - 0.1) - 0.5
+        return cls(allowed_mass=allowed_mass)
+
+    @cad_view("Knapsack CAD")
+    def cadview(self):
+        return VolumeModel(self.volmdlr_primitives()).babylon_data()
 
 @modelclass
 class KnapsackPackage(Knapsack):
@@ -213,47 +212,47 @@ class KnapsackPackage(Knapsack):
     def golds(self) -> int:
         return len([i for i in self.items.items if i.color == "gold"])
 
-    # def volmdlr_primitives(self):
-    #     primitives = super().volmdlr_primitives()
-    #     z_offset = 0
-    #     for item in self.items.items:
-    #         item_primitives = item.volmdlr_primitives(z_offset=z_offset)
-    #         primitives.extend(item_primitives)
-    #         z_offset += item.mass / 2 + 0.05
-    #     return primitives
-    #
-    # @cad_view("Knapsack Package CAD")
-    # def cadview(self):
-    #     return VolumeModel(self.volmdlr_primitives()).babylon_data()
-    #
-    # @plot_data_view("2D display for KnapsackPackage", picture=True)
-    # def display_2d(self):
-    #     primitives = []
-    #     y_offset = 0
-    #     for item in self.items.items:
-    #         primitive_groups = item.display_2d(y_offset=y_offset)
-    #         primitives.extend(primitive_groups.primitives)
-    #         y_offset += 1.1
-    #     text_style = TextStyle(text_color=BLACK,
-    #                            font_size=None,
-    #                            text_align_x="center",
-    #                            text_align_y="middle")
-    #     primitive1 = Text(comment=f"{self.mass} kg",
-    #                       position_x=0,
-    #                       position_y=-1.5,
-    #                       text_style=text_style,
-    #                       text_scaling=True,
-    #                       max_width=1,
-    #                       multi_lines=False)
-    #     primitive2 = Text(comment=f"{self.price} €",
-    #                       position_x=0,
-    #                       position_y=-2,
-    #                       text_style=text_style,
-    #                       text_scaling=True,
-    #                       max_width=1,
-    #                       multi_lines=False)
-    #     primitives.extend([primitive1, primitive2])
-    #     return PrimitiveGroup(primitives=primitives)
+    def volmdlr_primitives(self):
+        primitives = super().volmdlr_primitives()
+        z_offset = 0
+        for item in self.items.items:
+            item_primitives = item.volmdlr_primitives(z_offset=z_offset)
+            primitives.extend(item_primitives)
+            z_offset += item.mass / 2 + 0.05
+        return primitives
+
+    @cad_view("Knapsack Package CAD")
+    def cadview(self):
+        return VolumeModel(self.volmdlr_primitives()).babylon_data()
+
+    @plot_data_view("2D display for KnapsackPackage", picture=True)
+    def display_2d(self):
+        primitives = []
+        y_offset = 0
+        for item in self.items.items:
+            primitive_groups = item.display_2d(y_offset=y_offset)
+            primitives.extend(primitive_groups.primitives)
+            y_offset += 1.1
+        text_style = TextStyle(text_color=BLACK,
+                               font_size=None,
+                               text_align_x="center",
+                               text_align_y="middle")
+        primitive1 = Text(comment=f"{self.mass} kg",
+                          position_x=0,
+                          position_y=-1.5,
+                          text_style=text_style,
+                          text_scaling=True,
+                          max_width=1,
+                          multi_lines=False)
+        primitive2 = Text(comment=f"{self.price} €",
+                          position_x=0,
+                          position_y=-2,
+                          text_style=text_style,
+                          text_scaling=True,
+                          max_width=1,
+                          multi_lines=False)
+        primitives.extend([primitive1, primitive2])
+        return PrimitiveGroup(primitives=primitives)
 
 
 @modelclass
@@ -269,11 +268,11 @@ class ListKnapsackPackages(Model):
 
     _standalone_in_db = True
 
-    # @markdown_view("Generator markdown")
-    # def to_markdown(self, *args, **kwargs) -> str:
-    #     """Render a markdown of the object output type: string."""
-    #     dataset_object = dataset.Dataset(dessia_objects=self.knapsack_packages, name=self.name)
-    #     return dataset.Dataset.to_markdown(dataset_object, *args, **kwargs)
+    @markdown_view("Generator markdown")
+    def to_markdown(self, *args, **kwargs) -> str:
+        """Render a markdown of the object output type: string."""
+        dataset_object = dataset.Dataset(dessia_objects=self.knapsack_packages, name=self.name)
+        return dataset.Dataset.to_markdown(dataset_object, *args, **kwargs)
 
 
 @modelclass
