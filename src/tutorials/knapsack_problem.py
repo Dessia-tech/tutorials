6710518c257529740edd0562bcfa8ce84fe41e7a
Ghislain Jézéquel <jezequel@dessia.io>
2025-10-28 09:49:27 +0100

    fix(knapsack): wrong use of super with modelclasses

--- src/tutorials/knapsack_problem.py
+++ src/tutorials/knapsack_problem.py
@@ -8,6 +8,7 @@ from dessia_common.files import BinaryFile
 from datatools.dataset import Dataset
 from dessia_common.decorators import cad_view, plot_data_view, markdown_view
 from dessia_common.models.decorators import modelclass, model_property
+from models.contours import primitives
 from plot_data import PrimitiveGroup, SurfaceStyle, Text, TextStyle
 from plot_data.colors import BLACK, Color
 from volmdlr import O3D, X3D, Y3D, Z3D, Frame3D, Point2D
@@ -299,7 +300,7 @@ class KnapsackPackage(Knapsack):
         return len([i for i in self.items.items if i.color == "gold"])
 
     def volmdlr_primitives(self, reference_path: str = "#"):
-        primitives = super().volmdlr_primitives()
+        primitives = Knapsack.volmdlr_primitives(self, reference_path=reference_path)
         z_offset = 0
         for i, item in enumerate(self.items.items):
             item_primitives = item.volmdlr_primitives(z_offset=z_offset, reference_path=f"{reference_path}/items/{i}")
