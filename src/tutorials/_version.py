aab0b3f00dfde68792ed7acb1fce1528caf2a1c5
Lucas Launay <launay@dessia.tech>
2026-06-10 17:26:05 +0200

    fix: update datotools

--- src/tutorials/_version.py
+++ src/tutorials/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.3.1.dev1+g89693f88b.d20260608'
-__version_tuple__ = version_tuple = (0, 3, 1, 'dev1', 'g89693f88b.d20260608')
+__version__ = version = '0.3.1.dev2+gc0b9969f0'
+__version_tuple__ = version_tuple = (0, 3, 1, 'dev2', 'gc0b9969f0')
 
-__commit_id__ = commit_id = 'g89693f88b'
+__commit_id__ = commit_id = 'gc0b9969f0'
